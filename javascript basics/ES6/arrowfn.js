5d257b77166d596fa290f5b9a5564efc2900a754
anjaleena-mwt <anjaleena@mwttech.com>
2025-08-20 10:21:59 +0530

    Update arrowfn.js

--- javascript basics/ES6/arrowfn.js
+++ javascript basics/ES6/arrowfn.js
@@ -16,3 +16,37 @@ If you have zero parameters we have to add empty parentheses*/
 we can remove the return as well as the {} */
 const squaree = (number) => number*number;
 console.log(squaree(5));
+
+
+const jobs = [
+    { id: 1, isActive: true },
+    { id: 2, isActive: true },
+    { id: 3, isActive: false },
+];
+
+// to get only the active jobs
+// job is an object
+const activeJobs = jobs.filter(function(job) { return job.isActive; });
+//short way to write the above
+const activeJob = jobs.filter(job => job.isActive );
+console.log(activeJob);
+
+//Arrow functions and this
+// const person = {
+//     talk() {
+//         console.log('this', this);
+//     }
+// };
+// person.talk();
+
+// setTimeout 
+/* When we call a function as a standalone function outside 
+of an object by default this returns the window object */
+const person = {
+    talk() {
+        setTimeout(function() {
+            console.log("this", this);
+        }, 1000); 
+    }
+};
+person.talk();
