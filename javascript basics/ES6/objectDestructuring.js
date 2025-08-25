bf2a2b42602a3f561644693f1870324e24962361
anjaleena-mwt <anjaleena@mwttech.com>
2025-08-25 09:12:24 +0530

    Update objectDestructuring.js

--- javascript basics/ES6/objectDestructuring.js
+++ javascript basics/ES6/objectDestructuring.js
@@ -13,3 +13,19 @@ const { street, city, country } = address; //here no repetition of address., its
 
 // we can change the name of the const aka alias names
 const{ street:st } = address;
+
+
+// OBJECT DESTRUCTURING - extract values from arrays and objects, then assign them to variables in a convenient way
+// [] = to perform array destructuring
+// {} = to perform object destructuring
+
+let a = 1;
+let b = 2;
+
+[a,b] = [b,a];
+console.log(a);
+console.log(b);
+
+const colors = ["red", "green", "blue", "black", "white"];
+[colors[0], colors[4]] = [colors[4], colors[0]];
+console.log(colors);
